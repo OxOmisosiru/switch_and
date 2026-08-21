dbd7210a6040efcc6e934ce645cd2e81b3c3f17b
OxOmisosiru <miyaten42910@gmail.com>
2026-08-22 03:33:18 +0900

    change url

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
     <BrowserRouter>
       {/* ナビゲーションは削除しました（参加者に見せないため） */}
       <Routes>
-        <Route path="/" element={<Setup />} />
-        <Route path="/setup" element={<Setup />} />
-        <Route path="/play" element={<Play />} />
+        <Route path="/" element={<History />} />
+        <Route path="/Qscts66i_jbd" element={<Setup />} />
+        <Route path="/Z_W_78BzfcLQ" element={<Play />} />
         <Route path="/history" element={<History />} />
       </Routes>
     </BrowserRouter>
